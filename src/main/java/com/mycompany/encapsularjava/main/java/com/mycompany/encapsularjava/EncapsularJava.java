f80fcf362f2429c0d56a3b0fb3413b32645aba20
landerson malcher <leo.malcherr@gmail.com>
2026-04-29 21:59:54 -0300

    aula05: controle remoto - encapsulamento POO

---
+++ src/main/java/com/mycompany/encapsularjava/main/java/com/mycompany/encapsularjava/EncapsularJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.encapsularjava;
+
+public class EncapsularJava {
+
+    public static void main(String[] args) {
+       ControleRemoto c = new ControleRemoto();
+     c.ligar();
+       c.maisVolume();
+       c.abrirMenu();
+       c.fecharMenu();
+    }
+}
